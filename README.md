b921ceca4cb79b1b54206a82adea615c6ddf510d
Jane Yeh <jane.yeh@gmail.com>
2019-09-13 11:58:45 -0400

    Updated root readme; added image to practice app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# e2 Course work
+# Practice
++ By: Jane Yeh
++ Production URL: <http://p1.yourdomain.com>
+
+## Outside resources
+n/a
+
+## Notes for instructor
+n/a
--- practice2/index.php
+++ practice2/index.php
@@ -1,10 +1,11 @@
 <!doctype html> 
 <html lang='en'> 
 <head> 	
-<title>Practice Application</title> 	
-<meta charset='utf-8'> 
+        <title>Practice Application</title> 	
+        <meta charset='utf-8'> 
 </head> 
 <body> 	
-<h1>Practice Application</h1> 
+        <h1>Practice Application</h1> 
+        <img src='/images/hes-logo.png'>
 </body> 
 </html>
