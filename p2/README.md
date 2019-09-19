f87baf1c05a72c4bb60fb1adb996714646133a6a
Jane Yeh <jane.yeh@gmail.com>
2019-09-19 11:43:15 -0400

    p2 files

---
+++ p2/README.md
@@ -0,0 +1,9 @@
+# Project 2
++ By: *Jane Yeh*
++ Production URL: <http://p1.janenycxo.com>
+
+## Game planning
+Tic Tac Toe
+
+## Outside resources
+n/a
---
+++ p2/index.php
@@ -0,0 +1,13 @@
+  <!doctype html>
+  <html lang='en'>
+
+    <head>
+      <title>Project 2</title>
+      <meta charset='utf-8'>
+    </head>
+
+    <body>
+      <h1>Project 2 Multiplayer Tic Tac Toe Game</h1>
+    </body>
+
+  </html>
