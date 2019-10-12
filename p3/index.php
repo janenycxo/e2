2dccd0bfffb14e239ec5019fe64ad76cdd841ad3
Jane Yeh <jane.yeh@gmail.com>
2019-10-12 19:14:21 -0400

    updates

--- p3/index.php
+++ p3/index.php
@@ -24,27 +24,28 @@
   <body>
 
     <h1>Select between the 2 Dog Breeds to begin your agility training.</h1>
-    <h2>Agility training teaches your dog to navigate obstacles, such as weave through poles, negotiate tunnels, high and long jumps and balance on see-saws.
-    <br>Certain breeds excel at agility, while others struggle. However, all dogs can benefit from some type of obedience training.
-    <br>The computer chooses between the 2 dog breeds, and the player who selects the correct breed wins.
-    <br>It is a random selection game.</h2>
+    Agility training teaches your dog to navigate obstacles, such as weave through poles, negotiate tunnels, high and long jumps and balance on see-saws.
+    <br><br>Certain breeds excel at agility, while others struggle. However, all dogs can benefit from some type of obedience training.
+    <br><br>The computer chooses between the 2 dog breeds, and the player who selects the correct breed wins.
+    <br><br>It is a random selection game.
 
-    <br><br>
-    <h3>Rules for this game</h3>
+    <br>
+    <h2>Rules for this game</h2>
     1. This is a multi-player game.
     <br>2. Players choose a dog breed from the choices given. I selected the top 2 dogs breeds from the AKC list of most popular breeds for the last 5 years.
     <br>3. Players click the radio button to select.
     <br>4. The player whose breed matches the computer's wins.
-    <br><br>
+    <br>
+
     <h2>Mechanics</h2>
     The rules of the game are pretty simple. There are 2 choices to select from. Choose the breed you prefer and the computer will randomly select a breed as well.
     If your choice matches the computer, then you win.
     <br><br>
 
     <form method='GET' action='process.php'>
-      <h1>
+      <h2>
         <p>Choose the Dog Breed to begin your agility training. </p>
-      </h1>
+      </h2>
       <p>Hint: A man's best friend. </p>
       <div>
         <label for='name'>Name</label>
@@ -67,8 +68,7 @@
 
     </form>
 
-
-    
+   
 
   </body>
 
--- p3/process-controller.php
+++ p3/process-controller.php
@@ -2,9 +2,17 @@
 
 session_start();
 
-if ($_POST['answer'] == 'Labrador Retriever'){
-    $correct = 'true';
+$winner = null;
+$name = $_GET['name'];
+
+$choice = ['Labrador Retriever', 'German Shepherd']
+$breed =$_GET['dog breed'];
+$player1choice = $choice [rand (0,1)];
+$player2choice = $choice [rand (0,1)];
+if ($breed ==$player1choice) {
+    $winner = true;
 } else {
-    $results = 'Incorrect';
+    $winner = 'false';
 }
+
 
