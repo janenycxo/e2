de0cc498c3b1c26d27139fd7145cf80991956d3c
Jane Yeh <jane.yeh@gmail.com>
2019-10-13 18:02:58 -0400

    adding practice

---
+++ practice/README.md
@@ -0,0 +1,2 @@
+<h1>Practice Application</h1>
+<img src='/images/dog_180x.jpg' alt='Halle Pups Doggos'>
---
+++ practice/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang='en'>
+<head>
+	<title>Practice Application</title>
+	<meta charset='utf-8'>
+</head>
+<body>
+	<h1>Practice Application</h1>
+<img src='/images/dog_180x.jpg' alt='Halle Pups Doggos'>
+
+</body>
+</html>
