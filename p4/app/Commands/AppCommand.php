97ec83636006107dc5b5e4754ba10bc58a186f25
Jane Yeh <jane.yeh@gmail.com>
2019-12-12 18:17:30 -0500

    app commands

--- p4/app/Commands/AppCommand.php
+++ p4/app/Commands/AppCommand.php
@@ -1,30 +1,23 @@
 <?php
-
 namespace App\Commands;
-
 class AppCommand extends Command
 {
+public function test()
 
-    /**
-     *
-     */
-    public function test()
-    {       
-        
-        dump('It works! You invoked your first command.'); 
-            
-    }
+dump('It works! You invoked your first command.');
 
-    public function migrate()
-    {   
-        $this->app->db()->createTable('results', [
+}
+{
+public function migrate()
+
+
+$this->app->db()->createTable('results', [
             'winner' => 'int',
             'player1' => 'varchar(255)',
-            'player2' => 'varchar(255)',
-        ]);      
+            'player2' => 'varchar(255)',     
 dump('Migration complete.');
-       
-    }
+   
+  
 
     public function seed()
     {  
@@ -32,7 +25,7 @@ dump('Migration complete.');
 
         $faker = \Faker\Factory::create();
 
-        
+    }
         # Use a loop to create 10 results
         for ($i = 0; $i < 10; $i++) {
 
@@ -41,21 +34,18 @@ dump('Migration complete.');
                 'winner' => $faker->sentences (1,true),
                 'player1' => $faker->words (3,true),
                 'player2' => $faker->sentences (rand(3,10),
-            ];
 
-            
         # Insert the results
-        $this->app->db()->insert('results', $results);
-} 
-
-
-        dump('Seeds complete.');  
-    }
-
+        $this->app->db()->insert('results', '$result')';
+            }
+           
+           
+        dump('Seeds complete.');
+               
     public function fresh()
     {
     $this->migrate();
     $this->seed();
     }
 
-}
+        }
--- p4/app/Controllers/AppController.php
+++ p4/app/Controllers/AppController.php
@@ -4,35 +4,28 @@ namespace App\Controllers;
 
 class AppController extends Controller
 {
-
-    /**
-     *
-     */
     public function index()
     {       
-        
-        dump('It works! You invoked your first command.'); 
-            
+        return $this->app->view('index'); 
+      
     }
        
 
     public function results()
     {
-
-        { 
             $results = $this->app->db()->all('results');
-            return $this->app->view('results', ['results' => $results]);  
-        }
-
-        public function result()
+            return $this->app->view('results', ['results' => $results
+            ]);  
+    }
+    public function result()
     {        
             $id = $this->app->param('id');           
             $result = $this->app->db()->findById('results', $id);
             
             return $this->app->view('result', ['result' => $id]);
 
-
+    }
     $this->migrate();
     $this->seed();
     }
-    }
+    
